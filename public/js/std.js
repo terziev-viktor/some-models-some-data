aea558e494e74a9b797a27098e8c8a6b6b7c19c7
terziev-viktor <terzievviktor@gmail.com>
2018-04-02 22:15:00 +0300

    Font, Background and Sidenav

--- public/js/std.js
+++ public/js/std.js
@@ -1,1 +1,10 @@
 
+/* Open the sidenav */
+function openNav() {
+    $("#models_sidenav").width("100%");
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+    $("#models_sidenav").width("0");
+}
